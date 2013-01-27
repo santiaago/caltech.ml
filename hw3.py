f37cc0bcdb26e7a3812e6d6ad4e0b38ee8012ec0
Santiaago <santiago.ariassar@gmail.com>
2013-01-27 20:20:48 +0100

    hw3 q1 to 3 others do not need programing just pen and paper

---
+++ hw3.py
@@ -0,0 +1,29 @@
+
+from math import exp
+
+EPSILON = .05
+MAX_P_BOUND = 0.03
+
+def pbound(m,n):
+    return 2*m*exp(-2*(EPSILON**2)*n)
+
+def leastnumberofexamples(m = 1):
+    n = 0
+    while pbound(m,n) > 0.03:
+        n = n + 1
+        if n > 5000: break
+    return n
+      
+def tests():
+    #Generalization error: 1-2-3
+    print leastnumberofexamples(1)
+    print leastnumberofexamples(10)
+    print leastnumberofexamples(100)
+    #Break point: 4
+    #Growth function: 5
+    #Fun with intervals: 6-7-8
+    #Convex sets: The Triangle: 9
+    #Non convex sets: concentric circles: 10
+
+    
+           
